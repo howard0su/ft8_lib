d86afb2a8801d3aba43907fa11231b42be279e21
Howard Su <howard0su@gmail.com>
2024-12-19 08:25:39 +0800

    multi thread testing
    significant improve run_test.py perf

--- utils/run_tests.py
+++ utils/run_tests.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import sys, os, subprocess
+from concurrent.futures import ThreadPoolExecutor
 
 def parse(line):
     fields = line.strip().split()
@@ -31,39 +32,46 @@ is_ft4 = False
 if len(sys.argv) > 2 and sys.argv[2] == '-ft4':
     is_ft4 = True
 
-n_extra = 0
-n_missed = 0
-n_total = 0
-for wav_file, txt_file in zip(wav_files, txt_files):
-    if not os.path.isfile(txt_file): continue
-    print(wav_file)
+def process_file(wav_file, txt_file, is_ft4):
+    if not os.path.isfile(txt_file):
+        return (0, 0, 0)  # n_extra, n_missed, n_total
+
     cmd_args = ['./decode_ft8', wav_file]
     if is_ft4:
         cmd_args.append('-ft4')
-    result = subprocess.run(cmd_args, stdout=subprocess.PIPE)
+
+    result = subprocess.run(cmd_args, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
     result = result.stdout.decode('utf-8').split('\n')
     result = [parse(x) for x in result if len(x) > 0]
-    #print(result[0])
     result = set(result)
-    
+
     expected = open(txt_file).read().split('\n')
     expected = [parse(x) for x in expected if len(x) > 0]
-    #print(expected[0])
     expected = set(expected)
-    
+
     extra_decodes = result - expected
     missed_decodes = expected - result
-    print(len(result), '/', len(expected))
-    if len(extra_decodes) > 0:
-        print('Extra decodes: ', list(extra_decodes))
-    if len(missed_decodes) > 0:
-        print('Missed decodes: ', list(missed_decodes))
-    
-    n_total += len(expected)
-    n_extra += len(extra_decodes)
-    n_missed += len(missed_decodes)
-    
-    #break
+
+    return len(extra_decodes), len(missed_decodes), len(expected), wav_file, extra_decodes, missed_decodes
+
+n_extra = 0
+n_missed = 0
+n_total = 0
+with ThreadPoolExecutor() as executor:
+    futures = [executor.submit(process_file, wav_file, txt_file, is_ft4) 
+                for wav_file, txt_file in zip(wav_files, txt_files)]
+
+    for future in futures:
+        extra, missed, total, wav_file, extra_decodes, missed_decodes = future.result()
+        n_extra += extra
+        n_missed += missed
+        n_total += total    
+
+        print(wav_file)
+        if len(extra_decodes) > 0:
+            print('Extra decodes: ', list(extra_decodes))
+        if len(missed_decodes) > 0:
+            print('Missed decodes: ', list(missed_decodes))
 
 print('Total: %d, extra: %d (%.1f%%), missed: %d (%.1f%%)' % 
         (n_total, n_extra, 100.0*n_extra/n_total, n_missed, 100.0*n_missed/n_total))
